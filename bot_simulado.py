f778fc3038aa5d7ef8341204e512e798cb3b8e91
Nikolettka <97996029+Nikolettka@users.noreply.github.com>
2026-06-11 02:32:18 +0200

    Update bot_simulado.py

--- bot_simulado.py
+++ bot_simulado.py
@@ -170,4 +170,4 @@ if __name__ == "__main__":
     hilo_bot.start()
     
     # Arrancamos el servidor web en el puerto asignado por Railway
-    app.run_server(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
